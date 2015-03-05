bcf714d4cd13eeefcaf90e77b4935fca3b36ea8a
Nick Sergeant <nick@nicksergeant.com>
2015-03-05 00:29:42 -0500

    Everything hurts.

--- README.md
+++ README.md
@@ -4,14 +4,26 @@ slacktocracy
 A tiny little app to post Fitocracy workouts to Slack.
 
 1. `heroku create`
-2. `heroku config:add BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git`
-3. `heroku config:set PATH=$PATH:vendor/phantomjs/bin`
-4. `heroku config:add FITOCRACY_USERS_TO_FOLLOW=nicksergeant`
-5. `heroku config:add FITOCRACY_USERNAME=<username>`
-6. `heroku config:add FITOCRACY_PASSWORD=<password>`
-7. `heroku config:add SLACK_WEBHOOK_URLS=<slack_webhook_url>,<slack_webhook_url2>`
-8. `heroku addons:add mongolab:sandbox`
-9. `heroku addons:add scheduler:standard`
-10. `git push heroku`
-11. `heroku addons:open scheduler`
-12. Add job -> `node index.js` -> `Every 10 minutes`
+2. Log in to [Fitocracy](https://www.fitocracy.com/).
+3. Inspect any web request via the network inspector in your browser.
+4. From the `Request Headers` section, copy the entire cookie string.
+5. Seriously what the hell are you doing.
+6. You know what? Computers are absolutely terrible. If I had an API to work
+   with we wouldn't be in this mess.
+7. In fact, you should just create an entirely new user on Fitocracy before
+   you do this. I have no idea why Fitocracy hides user profiles behind auth
+   anyway - you don't have to be friends with someone to view their activity
+   stream.
+8. `heroku config:add FITOCRACY_COOKIE=<cookie_string>`
+9. Visit a user on Fitocracy who you want to track and open up the got-damn
+   inspector again.
+10. Type `profile_user_id` and hit enter.
+11. `heroku config:add FITOCRACY_USER_IDS_TO_FOLLOW=583852,583853`
+12. `heroku config:add SLACK_WEBHOOK_URLS=<slack_webhook_url>,<slack_webhook_url2>`
+13. `heroku addons:add mongolab:sandbox`
+14. `heroku addons:add scheduler:standard`
+15. `git push heroku`
+16. `heroku addons:open scheduler`
+17. Add job -> `node index.js` -> `Every 10 minutes`
+18. Just give up.
+19. Go be a [coconut salesman](http://isles.broker.is/).
--- index.js
+++ index.js
@@ -1,54 +1,44 @@
 'use strict';
 
 let Promise = require('bluebird');
+let request = require('request');
 let exec = require('child_process').exec;
-let sys = require('sys');
 
-const fitocracyUsersToFollow = process.env.FITOCRACY_USERS_TO_FOLLOW;
-const fitocracyUsername = process.env.FITOCRACY_USERNAME;
-const fitocracyPassword = process.env.FITOCRACY_PASSWORD;
+const fitocracyCookie = process.env.FITOCRACY_COOKIE;
+const fitocracyUserIdsToFollow = process.env.FITOCRACY_USER_IDS_TO_FOLLOW;
 const slackWebhookUrls = process.env.SLACK_WEBHOOK_URLS;
 
+function userActivityStream(userId) {
+  return new Promise(function(resolve, reject) {
+    request({
+      headers: {
+        Cookie: fitocracyCookie
+      },
+      url: 'https://www.fitocracy.com/activity_stream/0/?user_id=' + userId
+    }, function(error, response) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
 function postToSlack(workout) {
 
-  // let deferred = Q.defer();
-  // let sys = require('sys');
-  // let exec = require('child_process').exec;
+  let payload = {
+    username: 'Fitocracy',
+    text: 'Hay gurl.'
+  };
 
-  // function done(error, stdout, stderr) {
-  //   deferred.resolve();
-  // }
-
-  // let payload = {
-  //   "username": "Untappd",
-  //   "text": "" +
-  //     checkin.user.user_name + " is drinking <https://untappd.com/beer/" + checkin.beer.bid + "|" + checkin.beer.beer_name.replace('\'', '’') + "> " +
-  //     "(" + checkin.beer.beer_style.replace('\'', '’') + ", " + checkin.beer.beer_abv + "% ABV) " +
-  //     "by <https://untappd.com/brewery/" + checkin.brewery.brewery_id + "|" + checkin.brewery.brewery_name.replace('\'', '’') + ">.\n" + 
-  //     "He rated it a " + checkin.rating_score + 
-  //       (checkin.checkin_comment ?
-  //         " and said \"" + checkin.checkin_comment.replace('\'', '’') + "\". " :
-  //         ". ") +
-  //     "<https://untappd.com/user/" + checkin.user.user_name + "/checkin/" + checkin.checkin_id + "|Toast »>"
-  // };
-
-  // if (checkin.media.count) {
-  //   payload.attachments = [{
-  //     fallback: "Checkin photo",
-  //     image_url: checkin.media.items[0].photo.photo_img_lg
-  //   }];
-  // }
-
-  // let webhookUrls = slackWebhookUrls.split(',');
-
-  // webhookUrls.forEach((webhookUrl) -> {
-  //   exec('curl -X POST --data-urlencode \'payload=' + JSON.stringify(payload) + '\' ' + webhookUrl, done);
-  // });
-
-  // return deferred.promise;
-
-  console.log('hay');
+  slackWebhookUrls.split(',').forEach(function(webhookUrl) {
+    exec('curl -X POST --data-urlencode \'payload=' + JSON.stringify(payload) + '\' ' + webhookUrl);
+  });
 
 }
 
-postToSlack();
+fitocracyUserIdsToFollow.split(',').forEach(function(userId) {
+  userActivityStream(userId).then(function(workouts) {
+    console.log(workouts);
+  });
+});
