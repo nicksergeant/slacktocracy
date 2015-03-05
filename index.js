200b6449262b1bf409091a88c0da33dd7fb4ce02
Nick Sergeant <nick@nicksergeant.com>
2015-03-05 00:54:34 -0500

    Use jsdom for everything.

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 'use strict';
 
+var jsdom = require('jsdom');
 let Promise = require('bluebird');
-let request = require('request');
 let exec = require('child_process').exec;
 
 const fitocracyCookie = process.env.FITOCRACY_COOKIE;
 const fitocracyUserIdsToFollow = process.env.FITOCRACY_USER_IDS_TO_FOLLOW;
 const slackWebhookUrls = process.env.SLACK_WEBHOOK_URLS;
 
-function userActivityStream(userId) {
+function lastActivity(userId) {
   return new Promise(function(resolve, reject) {
-    request({
-      headers: {
-        Cookie: fitocracyCookie
-      },
-      url: 'https://www.fitocracy.com/activity_stream/0/?user_id=' + userId
-    }, function(error, response) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(response);
+    jsdom.env({
+      url: 'https://www.fitocracy.com/activity_stream/0/?user_id=' + userId,
+      headers: { cookie: fitocracyCookie },
+      scripts: ['http://code.jquery.com/jquery.js'],
+      done: function (error, window) {
+
+        if (error) return reject(error);
+
+        let $ = window.$;
+        let workout = $('.stream_item').eq(0);
+
+        let author = $('.stream-author', workout).text().trim();
+        let type = $('.stream-type', workout).text().trim();
+
+        resolve({
+          title: `${author} ${type}`
+        });
+
       }
     });
   });
@@ -38,7 +46,7 @@ function postToSlack(workout) {
 }
 
 fitocracyUserIdsToFollow.split(',').forEach(function(userId) {
-  userActivityStream(userId).then(function(workouts) {
-    console.log(workouts);
+  lastActivity(userId).then(function(workout) {
+    console.log(workout);
   });
 });
